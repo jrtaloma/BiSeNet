7b29cf5a07fadb54e18cb79fd931a75141604f02
Redemptor Jr Laceda Taloma <32417822+jrtaloma@users.noreply.github.com>
2021-11-05 12:28:13 +0100

    binary labels: 0, 255

--- lib/t1.py
+++ lib/t1.py
@@ -25,7 +25,7 @@ class T1(BaseDataset):
         #self.lb_ignore = 255
 
         ## label mapping
-        labels = list(np.arange(self.n_cats))
+        labels = [0, 255]
         self.lb_map = np.arange(256)
         for ind in labels:
             self.lb_map[ind] = labels.index(ind)
